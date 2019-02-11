f2c78ca3bf745085887d8627c00e4e2a8b13100a
sony <sony@sony-VAIO>
2019-02-11 11:45:08 +0530

    commit2

---
+++ src/selenium/testgit.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testgit {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("hi...bye...",true);
+	}
+
+}
